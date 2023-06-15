ed374bd8aafc12c58a3864418b806dd19bb902f3
Yergeni Cabranes <yergenis@gmail.com>
2023-06-15 13:58:42 -0400

    feat: add new skills

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -5,70 +5,84 @@ import html from "../assets/tech/html.png";
 import css from "../assets/tech/css.png";
 import mui from "../assets/tech/mui.png";
 import javascript from "../assets/tech/javascript.png";
-import reactImage from "../assets/tech/react.png";
+import react from "../assets/tech/react.png";
+import typescript from "../assets/tech/typescript.png";
 import graphql from "../assets/tech/graphql.png";
 import github from "../assets/tech/github.png";
 import tailwind from "../assets/tech/tailwind.png";
+import mernStack from "../assets/tech/mernStack.png";
 import node from "../assets/tech/node.png";
 
-function Experience() {
-  const techs = [
-    {
-      id: 1,
-      src: html,
-      title: "HTML",
-      style: "shadow-orange-500",
-    },
-    {
-      id: 2,
-      src: css,
-      title: "CSS",
-      style: "shadow-blue-500",
-    },
-    {
-      id: 3,
-      src: javascript,
-      title: "JavaScript",
-      style: "shadow-yellow-500",
-    },
-    {
-      id: 4,
-      src: reactImage,
-      title: "React",
-      style: "shadow-sky-500",
-    },
-    {
-      id: 5,
-      src: mui,
-      title: "Material UI",
-      style: "shadow-blue-600",
-    },
-    {
-      id: 6,
-      src: tailwind,
-      title: "Tailwind",
-      style: "shadow-sky-400",
-    },
-    {
-      id: 7,
-      src: graphql,
-      title: "GraphQL",
-      style: "shadow-pink-400",
-    },
-    {
-      id: 8,
-      src: github,
-      title: "GitHub",
-      style: "shadow-gray-400",
-    },
-    {
-      id: 9,
-      src: node,
-      title: "Node JS",
-      style: "shadow-lime-400",
-    },
-  ];
+const techs = [
+	{
+		id: "html",
+		src: html,
+		title: "HTML",
+		style: "shadow-orange-500",
+	},
+	{
+		id: "css",
+		src: css,
+		title: "CSS",
+		style: "shadow-blue-500",
+	},
+	{
+		id: "js",
+		src: javascript,
+		title: "JavaScript",
+		style: "shadow-yellow-500",
+	},
+	{
+		id: "react",
+		src: react,
+		title: "React",
+		style: "shadow-sky-500",
+	},
+	{
+		id: "ts",
+		src: typescript,
+		title: "TypeScript",
+		style: "shadow-blue-400",
+	},
+	{
+		id: "mui",
+		src: mui,
+		title: "Material UI",
+		style: "shadow-blue-600",
+	},
+	{
+		id: "taildwindcss",
+		src: tailwind,
+		title: "Tailwind",
+		style: "shadow-sky-400",
+	},
+	{
+		id: "gql",
+		src: graphql,
+		title: "GraphQL",
+		style: "shadow-pink-400",
+	},
+	{
+		id: "git",
+		src: github,
+		title: "Git",
+		style: "shadow-red-400",
+	},
+	{
+		id: "node",
+		src: node,
+		title: "Node JS",
+		style: "shadow-lime-400",
+	},
+	{
+		id: "mern",
+		src: mernStack,
+		title: "MERN Stack",
+		style: "shadow-orange-300",
+	},
+];
 
+function Experience() {
 	return (
 		<Element
 			name="experience"
@@ -79,17 +93,17 @@ function Experience() {
 					<p className="py-6">These are some technologies I've worked with</p>
 				</SectionTitle>
 
-        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8">
-          {techs.map(({ id, src, title, style }) => (
-            <div
-              key={id}
-              className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
-            >
-              <img src={src} alt="" className="w-20 mx-auto" />
-              <p className="mt-4">{title}</p>
-            </div>
-          ))}
-        </div>
+				<div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8">
+					{techs.map(({ id, src, title, style }) => (
+						<div
+							key={id}
+							className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
+						>
+							<img src={src} alt="" className="w-20 mx-auto" />
+							<p className="mt-4">{title}</p>
+						</div>
+					))}
+				</div>
 			</div>
 		</Element>
 	);
--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -20,104 +20,105 @@ import SectionTitle from "./common/SectionTitle";
 import { FaCode } from "react-icons/fa";
 import { FaEye } from "react-icons/fa";
 
+const projects = [
+	{
+		src: parkosWebpay,
+		title: "ParkOS Web Pay",
+		alt: "Parkos Webpay",
+		demoLink: "https://pay.reefparking.com",
+	},
+	{
+		src: noProtectCMS,
+		title: "NO Protect CMS",
+		alt: "noProtectCMS",
+		demoLink: "https://support.nightowlsp.com/hc/en-us/articles/360057114614",
+	},
+	{
+		src: noProtectWebPortal,
+		title: "NO Protect Web Portal",
+		alt: "NO Protect Web Portal",
+		demoLink: "https://web-protect-staging.nowlsp.com/landing/",
+	},
+	{
+		src: mernAuthBoilerplate,
+		title: "MERN Auth Boilerplate",
+		alt: "MERN Auth",
+		demoLink: "https://yero-mern-auth-boilerplate.netlify.app/",
+		codeLink: "https://github.com/Yergeni/mern-auth-boilerplate",
+	},
+	{
+		src: twitterClone,
+		title: "Twitter Clone w/ T3 Stack",
+		alt: "Twitter Clone",
+		demoLink: "https://yero-twitter-clone.netlify.app/",
+		codeLink: "https://github.com/Yergeni/twitter-clone",
+	},
+	// {
+	// 	src: discordClone,
+	// 	title: "Basic Discord Clone",
+	// 	alt: "Discord Clone",
+	// 	demoLink: "https://yero-discord-clone.netlify.app/",
+	// 	codeLink: "https://github.com/Yergeni/discord-clone",
+	// },
+	{
+		src: crownStudy,
+		title: "Crown Clothes",
+		alt: "Crown Study",
+		demoLink: "https://crwn-study.herokuapp.com/",
+		codeLink: "https://github.com/Yergeni/crwn-clothing",
+	},
+	{
+		src: hangmanGame,
+		title: "Practice & Fun (Hangman)",
+		alt: "Hangman",
+		demoLink: "https://yero-hangman.netlify.app/",
+		codeLink: "https://github.com/Yergeni/hangman",
+	},
+	{
+		src: notes,
+		title: "Practice CRUD operations",
+		alt: "Notes",
+		demoLink: "https://yero-notes.netlify.app/",
+		codeLink: "https://github.com/Yergeni/notes-with-tags",
+	},
+	{
+		src: covid19Tracker,
+		title: "COVID 19 Tracker",
+		alt: "Covid19 Tracker",
+		demoLink: "https://covi19-tracker-a9817.web.app/",
+		codeLink: "https://github.com/Yergeni/covi19-tracker",
+	},
+	{
+		src: lotrInfo,
+		title: "LOTR Info",
+		alt: "LOTR Info",
+		demoLink: "https://yero-lotr-info.netlify.app/",
+		codeLink: "https://github.com/Yergeni/liblab_project",
+	},
+	{
+		src: coduxMovies,
+		title: "Codux Movies",
+		alt: "Codux Movies",
+		demoLink: "https://yero-codux-movies.netlify.app/",
+		codeLink: "https://github.com/Yergeni/codux-movies",
+	},
+	{
+		src: netflixClone,
+		title: "Basic Netflix Clone",
+		alt: "Netflix Clone",
+		demoLink: "https://netflix-clone-e03a2.web.app/",
+		codeLink: "https://github.com/Yergeni/basic-netflix-clone",
+	},
+	{
+		src: amazonClone,
+		title: "Basic Amazon Clone",
+		alt: "amazonClone",
+		demoLink: "https://clone-4a1c8.web.app/",
+		codeLink: "https://github.com/Yergeni/amazon-clone",
+	},
+];
+
 function Portfolio() {
-	const projects = [
-		{
-			src: parkosWebpay,
-			title: "ParkOS Web Pay",
-			alt: "Parkos Webpay",
-			demoLink: "https://pay.reefparking.com",
-		},
-		{
-			src: noProtectCMS,
-			title: "NO Protect CMS",
-			alt: "noProtectCMS",
-			demoLink: "https://support.nightowlsp.com/hc/en-us/articles/360057114614",
-		},
-		{
-			src: noProtectWebPortal,
-			title: "NO Protect Web Portal",
-			alt: "NO Protect Web Portal",
-			demoLink: "https://web-protect-staging.nowlsp.com/landing/",
-		},
-		{
-			src: mernAuthBoilerplate,
-			title: "MERN Auth Boilerplate",
-			alt: "MERN Auth",
-			demoLink: "https://yero-mern-auth-boilerplate.netlify.app/",
-			codeLink: "https://github.com/Yergeni/mern-auth-boilerplate",
-		},
-		{
-			src: twitterClone,
-			title: "Twitter Clone w/ T3 Stack",
-			alt: "Twitter Clone",
-			demoLink: "https://yero-twitter-clone.netlify.app/",
-			codeLink: "https://github.com/Yergeni/twitter-clone",
-		},
-		// {
-		// 	src: discordClone,
-		// 	title: "Basic Discord Clone",
-		// 	alt: "Discord Clone",
-		// 	demoLink: "https://yero-discord-clone.netlify.app/",
-		// 	codeLink: "https://github.com/Yergeni/discord-clone",
-		// },
-		{
-			src: crownStudy,
-			title: "Crown Clothes",
-			alt: "Crown Study",
-			demoLink: "https://crwn-study.herokuapp.com/",
-			codeLink: "https://github.com/Yergeni/crwn-clothing",
-		},
-		{
-			src: hangmanGame,
-			title: "Practice & Fun (Hangman)",
-			alt: "Hangman",
-			demoLink: "https://yero-hangman.netlify.app/",
-			codeLink: "https://github.com/Yergeni/hangman",
-		},
-		{
-			src: notes,
-			title: "Practice CRUD operations",
-			alt: "Notes",
-			demoLink: "https://yero-notes.netlify.app/",
-			codeLink: "https://github.com/Yergeni/notes-with-tags",
-		},
-		{
-			src: covid19Tracker,
-			title: "COVID 19 Tracker",
-			alt: "Covid19 Tracker",
-			demoLink: "https://covi19-tracker-a9817.web.app/",
-			codeLink: "https://github.com/Yergeni/covi19-tracker",
-		},
-		{
-			src: lotrInfo,
-			title: "LOTR Info",
-			alt: "LOTR Info",
-			demoLink: "https://yero-lotr-info.netlify.app/",
-			codeLink: "https://github.com/Yergeni/liblab_project",
-		},
-		{
-			src: coduxMovies,
-			title: "Codux Movies",
-			alt: "Codux Movies",
-			demoLink: "https://yero-codux-movies.netlify.app/",
-			codeLink: "https://github.com/Yergeni/codux-movies",
-		},
-		{
-			src: netflixClone,
-			title: "Basic Netflix Clone",
-			alt: "Netflix Clone",
-			demoLink: "https://netflix-clone-e03a2.web.app/",
-			codeLink: "https://github.com/Yergeni/basic-netflix-clone",
-		},
-		{
-			src: amazonClone,
-			title: "Basic Amazon Clone",
-			alt: "amazonClone",
-			demoLink: "https://clone-4a1c8.web.app/",
-			codeLink: "https://github.com/Yergeni/amazon-clone",
-		},
-	];
 
 	return (
 		<Element
